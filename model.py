4269355bb98c315f4e869122380dd5e73023066f
JuanManuelMartinez <juanyaca2006@gmail.com>
2025-11-21 17:43:23 -0500

    Ajustes

--- model.py
+++
@@ -1,9 +0,0 @@
-from langchain.agents import create_agent
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-import tools
-
-agent =ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=0
-)
